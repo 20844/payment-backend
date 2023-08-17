0cbc9d44cb504c576759eade88e99eee7096ce92
pengyang <20844@qq.com>
2023-08-18 06:53:45 +0800

    定义统一结果

--- src/main/java/com/py/paymentbackend/controller/ProductController.java
+++ src/main/java/com/py/paymentbackend/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.py.paymentbackend.controller;
 
+import com.py.paymentbackend.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @Api(tags = "商品管理")
 @RestController
@@ -13,7 +16,7 @@ public class ProductController {
 
     @ApiOperation("测试接口")
     @GetMapping("/test")
-    public String test(){
-        return "Hello";
+    public R test(){
+        return R.ok().data("message", "hello").data("now",new Date());
     }
 }
